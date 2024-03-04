2142026a0b9c630a066af76503b55648cd29314e
Matthias Horgen <matthiaskrh123@gmail.com>
2024-03-03 19:53:10 -0800

    Intro

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -28,104 +28,110 @@ public class PlayerController : MonoBehaviour
     
     public float jumpVelocity = 10;
 
+    public bool isDead = false;
+
     void Start()
     {
         
     }
 
     void Update(){
-        // Flag visual
-        flagSprite.SetActive(hasFlag);
+        if(!isDead){
+            // Flag visual
+            flagSprite.SetActive(hasFlag);
 
-        // Flag throwing
-        Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mouseDir = (mousePosition - (Vector2)transform.position).normalized;
+            // Flag throwing
+            Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mouseDir = (mousePosition - (Vector2)transform.position).normalized;
+            if(Input.GetKey(KeyCode.Mouse0) && hasFlag){
+                hasFlag = false;
+                ignoreNextFlagCollision = true;
+                flagInstance = Instantiate(flagPrefab, transform.position, transform.rotation);
+                flagInstance.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity + (mouseDir * throwVelocity);
+            }
 
-        if(Input.GetKey(KeyCode.Mouse0) && hasFlag){
-            hasFlag = false;
-            ignoreNextFlagCollision = true;
-            flagInstance = Instantiate(flagPrefab, transform.position, transform.rotation);
-            flagInstance.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity + (mouseDir * throwVelocity);
+            // Looking
+            float angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
+            headSprite.transform.eulerAngles = new Vector3(0, 0, angle);
+            
+            if(Input.GetKey(jump) && isGrounded){
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpVelocity);
+            }
         }
-
-        // Looking
-        float angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
-        headSprite.transform.eulerAngles = new Vector3(0, 0, angle);
-        
-        if(Input.GetKey(jump) && isGrounded){
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpVelocity);
-        }
-
     }
 
     void FixedUpdate()
     {
-        // Ground check
-        isGrounded = false;
-        RaycastHit2D hit1 = Physics2D.Raycast(groundCheck1.transform.position, Vector3.down, 0.1f);
-        if(hit1.collider != null){
-            if(hit1.collider.transform.tag == "Ground"){
-                isGrounded = true;
+        if(!isDead){
+            // Ground check
+            isGrounded = false;
+            RaycastHit2D hit1 = Physics2D.Raycast(groundCheck1.transform.position, Vector3.down, 0.1f);
+            if(hit1.collider != null){
+                if(hit1.collider.transform.tag == "Ground"){
+                    isGrounded = true;
+                }
             }
-        }
-        RaycastHit2D hit2 = Physics2D.Raycast(groundCheck2.transform.position, Vector3.down, 0.1f);
-        if(hit2.collider != null){
-            if(hit2.collider.transform.tag == "Ground"){
-                isGrounded = true;
+            RaycastHit2D hit2 = Physics2D.Raycast(groundCheck2.transform.position, Vector3.down, 0.1f);
+            if(hit2.collider != null){
+                if(hit2.collider.transform.tag == "Ground"){
+                    isGrounded = true;
+                }
             }
-        }
 
-        // if(isGrounded){
-        //     if(!Input.GetKey(left) && !Input.GetKey(right)){
-        //         GetComponent<Rigidbody2D>().drag = groundDragNoInput;
-        //     } else {
-        //         GetComponent<Rigidbody2D>().drag = groundDrag;
-        //     }
-        // } else {
-        //     GetComponent<Rigidbody2D>().drag = airDrag;
-        // }
+            // if(isGrounded){
+            //     if(!Input.GetKey(left) && !Input.GetKey(right)){
+            //         GetComponent<Rigidbody2D>().drag = groundDragNoInput;
+            //     } else {
+            //         GetComponent<Rigidbody2D>().drag = groundDrag;
+            //     }
+            // } else {
+            //     GetComponent<Rigidbody2D>().drag = airDrag;
+            // }
 
-        // Physics Controls
-        float horizontalVel = GetComponent<Rigidbody2D>().velocity.x;
-        float horizontalForce = horizontalAccel * GetComponent<Rigidbody2D>().mass;
-        if(Input.GetKey(left)){
-            if(horizontalVel >= -maxHorizontalSpeed){
-                GetComponent<Rigidbody2D>().AddForce(Vector2.left * horizontalForce);
+            // Physics Controls
+            float horizontalVel = GetComponent<Rigidbody2D>().velocity.x;
+            float horizontalForce = horizontalAccel * GetComponent<Rigidbody2D>().mass;
+            if(Input.GetKey(left)){
+                if(horizontalVel >= -maxHorizontalSpeed){
+                    GetComponent<Rigidbody2D>().AddForce(Vector2.left * horizontalForce);
+                }
             }
-        }
 
-        if(Input.GetKey(right)){
-            if(horizontalVel <= maxHorizontalSpeed){
+            if(Input.GetKey(right)){
+                if(horizontalVel <= maxHorizontalSpeed){
+                    GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalForce);
+                }
+            }
+
+            // Slowing down automatically on ground (friction)
+            float dragThreshold = 0.5f;
+            if(!Input.GetKey(left) && !Input.GetKey(right) && isGrounded){
+                if(horizontalVel > dragThreshold){
+                    GetComponent<Rigidbody2D>().AddForce(Vector2.left * horizontalForce);
+                } else if (horizontalVel < -dragThreshold){
                 GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalForce);
-            }
-        }
+                }
 
-        // Slowing down automatically on ground (friction)
-        if(!Input.GetKey(left) && !Input.GetKey(right) && isGrounded){
-            if(horizontalVel > 0){
-                GetComponent<Rigidbody2D>().AddForce(Vector2.left * horizontalForce);
-            } else if (horizontalVel < 0){
-               GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalForce);
-            }
-
-            if(Mathf.Abs(horizontalVel) < 0.1f){
-                GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+                if(Mathf.Abs(horizontalVel) < dragThreshold){
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+                }
             }
         }
     }
 
     IEnumerator End()
     {
-        float elapsedTime = 0;
-        while(elapsedTime < 3.0f){
-            yield return null;
-            elapsedTime += Time.unscaledDeltaTime;
-            float ratio = elapsedTime / 3.0f;
-            Time.timeScale = Mathf.Lerp(1, 0, ratio);
-        }
+        // float duration = 1.0f;
+        // float elapsedTime = 0;
+        // while(elapsedTime < duration){
+        //     yield return null;
+        //     elapsedTime += Time.unscaledDeltaTime;
+        //     float ratio = elapsedTime / duration;
+        //     Time.timeScale = Mathf.Lerp(1, 0, ratio);
+        // }
 
-        Time.timeScale = 1;
-
+        // Time.timeScale = 1;
+        yield return new WaitForSeconds(1.0f);
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
     }
@@ -154,6 +160,7 @@ public class PlayerController : MonoBehaviour
                 visuals.SetActive(false);
                 GetComponent<Rigidbody2D>().simulated = false;
                 StartCoroutine(End());
+                isDead = true;
             }
         }
     }
